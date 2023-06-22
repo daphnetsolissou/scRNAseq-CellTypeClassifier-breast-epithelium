13d0f40656d215c0912bb9c53fc863fc3fb647d3
Daphne <daphnetsolissou@gmail.com>
2023-06-22 20:40:00 +0300

    Change svm algorithm to optimize and add outer cv

--- model_optimization.py
+++ model_optimization.py
@@ -4,41 +4,43 @@ import sys
 from optuna.samplers import TPESampler
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedKFold, cross_val_score
-from sklearn.multiclass import OneVsRestClassifier, OneVsOneClassifier
+from sklearn.multiclass import OneVsRestClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC, LinearSVC
 
-classifiers = {0: 'svm',
-               1: 'lr',
-               2: 'gnb'
-               }
+import warnings
+warnings.filterwarnings('ignore')
+optuna.logging.set_verbosity(optuna.logging.WARNING)
 
 
 def objective(trial, train_x, train_y, clf_name, seed):
     estimator = define_new_model_estimator(clf_name, trial, seed)
     # -- Make a pipeline
     # pipeline = make_pipeline(estimator)
-    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
+    cv = StratifiedKFold(n_splits=3, shuffle=True, random_state=seed)
+    if clf_name == 'svm':
+        scaler = StandardScaler()
+        train_x = scaler.fit_transform(train_x, train_y)
     # -- Evaluate the score by cross-validation
-    score = cross_val_score(estimator, train_x, train_y, scoring='f1_weighted', cv=cv, n_jobs=-1)
-    f1 = score.mean()  # calculate the mean
-    return f1
+    score = cross_val_score(estimator, train_x, train_y, scoring='balanced_accuracy', cv=cv, n_jobs=-1)
+    ba = score.mean()  # calculate the mean
+    return ba
 
 
 def define_new_model_estimator(clf_name, trial, seed):
     if clf_name == 'svm':
-        c_svm = trial.suggest_float('c_svm', 0.1, 100, log=True)
-        # c_tol = trial.suggest_float('c_tol', 1e-6, 1e-2, log=True)
-        c_loss = trial.suggest_categorical('c_loss', ['hinge', 'squared_hinge'])
+        c_svm = trial.suggest_float('c_svm', 1, 10, log=True)
+        gamma_svm = trial.suggest_float("gamma_svm", 0.01, 0.1, log=True)
 
-        estimator = OneVsRestClassifier(LinearSVC(loss=c_loss, C=c_svm, random_state=seed,
-                                                  multi_class='ovr', max_iter=100000))
+        estimator = OneVsRestClassifier(SVC(kernel='rbf', gamma=gamma_svm, C=c_svm, random_state=seed, max_iter=1000),
+                                        n_jobs=-1)
     elif clf_name == 'lr':
         lr_solver = trial.suggest_categorical('lr_solver', ['newton-cg', 'lbfgs', 'newton-cholesky', 'sag', 'saga'])
         lr_c = trial.suggest_float('lr_c', 0.01, 10, log=True)
 
-        estimator = LogisticRegression(solver=lr_solver, C=lr_c, random_state=seed, penalty='l2', multi_class='ovr',
-                                       max_iter=100000, n_jobs=-1)
+        estimator = LogisticRegression(solver=lr_solver, C=lr_c, random_state=seed, penalty='l2',
+                                       multi_class='ovr', max_iter=1000, n_jobs=-1)
     elif clf_name == 'gnb':
         gnb_var_smoothing = trial.suggest_float("gnb_var_smoothing", 1e-10, 1e-6, log=True)
 
@@ -67,22 +69,22 @@ def optimize_one_model(data_x, data_y, clf_name, seed):
     my_objective = lambda trial: objective(trial, data_x, data_y, clf_name, seed)
     sampler = TPESampler(seed=seed)
     study = optuna.create_study(direction='maximize', sampler=sampler)
-    study.optimize(my_objective, n_trials=100, show_progress_bar=True, n_jobs=-1)
+    study.optimize(my_objective, n_trials=25, show_progress_bar=True, n_jobs=-1)
 
     best_estimator = retrain_best_model_objective(clf_name, study.best_trial, data_x, data_y, seed)
-
     best_params = study.best_params
+
     return best_estimator, best_params
 
 
-def run_optimization_all_classifiers(x, y, seed=148):
-    estimators_dict = {}
-    params_dict = {}
-    for key, name in classifiers.items():
-        print(f'Starting optimization for classifier {name}')
-        # res_df = nested_cv(clf, hyperparam_grids[key], x, y)
-        best_estimator, best_params = optimize_one_model(x, y, name, seed)
-        estimators_dict[name] = best_estimator
-        params_dict[name] = best_params
-        # break
-    return estimators_dict, params_dict
+# def run_optimization_all_classifiers(x, y, seed=148):
+#     estimators_dict = {}
+#     params_dict = {}
+#     for key, name in classifiers.items():
+#         print(f'Starting optimization for classifier {name}')
+#         # res_df = nested_cv(clf, hyperparam_grids[key], x, y)
+#         best_estimator, best_params = optimize_one_model(x, y, name, seed)
+#         estimators_dict[name] = best_estimator
+#         params_dict[name] = best_params
+#         # break
+#     return estimators_dict, params_dict
--- preprocessing.py
+++ preprocessing.py
@@ -3,16 +3,15 @@ import numpy as np
 
 def make_label_mapping_dict(data):
     labels = list(set(data['cluster_label'].tolist()))
-    celltype_to_int = {name:i for i, name in enumerate(labels)}
+    celltype_to_int = {name: i for i, name in enumerate(labels)}
     return celltype_to_int
 
 
 def split_X_y(data, label_name='cluster_label'):
     columns = list(data.columns)
     # print(columns)
-    columns.remove('cluster_label')
-    print(columns)
-    X = data[columns]
+    columns.remove(label_name)
+    X = data[columns].copy()
 
     celltype_to_int = make_label_mapping_dict(data)
     y = np.asarray([celltype_to_int[type] for type in data['cluster_label'].tolist()])
